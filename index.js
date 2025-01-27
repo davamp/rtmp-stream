72ecb443ba8ec5f24853c9ead8077b5c77725836
davamp <yanoda8@gmail.com>
2025-01-27 14:53:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,20 +19,19 @@ const rtmpConfig = {
 };
 
 const transformationConfig = {
-  ffmpeg: ffmpegPath,
-  tasks: [
-    {
-      app: 'live',
-      hls: true,
-      hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-      hlsKeep: true, // to prevent hls file delete after end the stream
-      dash: true,
-      dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
-      dashKeep: true // to prevent dash file delete after end the stream
-    }
-  ],
-  MediaRoot: "./media",
-};
+    ffmpeg: ffmpegPath,
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        dashKeep: true // to prevent dash file delete after end the stream
+      }
+    ]
+  };
 const fissionConfig = {
   ffmpeg: ffmpegPath,
   tasks: [
